efba90c8b7bffd69106df7e0ea5396b10f4c2804
Devang Sharma <devshu25@gmail.com>
2022-02-16 16:38:02 +0000

    Updated Search Function

--- Projects with HTML, CSS and JS/Youtube Clone/index.js
+++ Projects with HTML, CSS and JS/Youtube Clone/index.js
@@ -9,7 +9,7 @@ display()
 - Show/Display Videos on Homepage
 - Based Upon Most Popular Videos
 - Should be Called Directly WITHOUT Any Event 
-(No Hover,No Click , No On Click)
+(No Hover, No Click , No On Click)
 - On Page Load
 
 
@@ -42,19 +42,19 @@ display();
 async function searchVideos(){
     document.getElementById("videodiv").innerHTML=""; // Empty 
 
-    let video = document.getElementById("video").value; // Search String/ Query Stri
+    let query = document.getElementById("video").value; // Search String/ Query Stri
 
 //search for videos
-let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${video}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
+let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
 let data = await res.json();
 
-for({
-id:{videoId}}of data.items){
+for({id:{videoId}}of data.items)
+{
 let videodiv= document.createElement("iframe")
 videodiv.setAttribute("class","mons")
 videodiv.src = `https://www.youtube.com/embed/${videoId}`;
 videodiv.allow = 'fullscreen'
 div.append(videodiv)
-
 }
+
 }
