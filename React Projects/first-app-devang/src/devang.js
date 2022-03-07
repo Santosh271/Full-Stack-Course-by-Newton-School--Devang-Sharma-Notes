941c7912e702d6214150acf5f47a7db6ce9db47e
Devang Sharma <devshu25@gmail.com>
2022-03-07 16:42:03 +0000

    Components and Structure

--- React Projects/first-app-devang/src/devang.js
+++ React Projects/first-app-devang/src/devang.js
@@ -1,7 +1,30 @@
+// RFC
+
 import React from 'react'
 
 export default function devang() {
   return (
     <div>devang</div>
   )
+}
+
+// RFAC
+import React from 'react'
+
+export const devang = () => {
+  return (
+    <div>devang</div>
+  )
+}
+
+
+// RCC
+import React, { Component } from 'react';
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div> textInComponent </div>
+    );
+  }
 }
